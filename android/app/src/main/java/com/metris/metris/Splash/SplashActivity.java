305161e88c76ebb265f99539a4a95586a5d939be
rrdea <deaannisayanti@gmail.com>
2018-08-24 08:54:23 +0700

    add splash screen

---
+++ android/app/src/main/java/com/metris/metris/Splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.metris.metris.Splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.metris.metris.R;
+import com.metris.metris.Welcome.WelcomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcomeIntent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(welcomeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
--- android/app/src/main/java/com/metris/metris/Welcome/WelcomeActivity.java
+++ android/app/src/main/java/com/metris/metris/Welcome/WelcomeActivity.java
@@ -1,13 +1,15 @@
-package com.metris.metris;
+package com.metris.metris.Welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.metris.metris.R;
+
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_welcome);
     }
 }
